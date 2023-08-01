ed5cdf0ff4a0f3c4d116b07514c0764ff0907974
isaaccleopas <isaaccleopas@gmail.com>
2023-08-01 11:21:50 +0000

    tasks 3 and 4

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-import getListStudents from './0-get_list_students.js';
+import getListStudents from './0-get_list_students';
 
 export default function getStudentsByLocation(students = getListStudents(), city) {
   return students.filter((student) => student.location === city);
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+import getListStudents from './0-get_list_students';
+
+export default function updateStudentGradeByCity(students = getListStudents(), city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
