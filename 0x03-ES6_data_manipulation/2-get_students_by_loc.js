7af91ae021c4e56f6b5abbd477348aa604452e74
isaaccleopas <isaaccleopas@gmail.com>
2023-08-01 10:43:20 +0000

    task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,5 @@
+import getListStudents from './0-get_list_students.js';
+
+export default function getStudentsByLocation(students = getListStudents(), city) {
+  return students.filter((student) => student.location === city);
+}
