9f4d4ab4da94e88cdc853af7a8f617b31f5d1799
isaaccleopas <isaaccleopas@gmail.com>
2023-08-01 12:34:50 +0000

    corrections for task 8 and advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,10 @@
 const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-  weakMap.set(endpoint, count + 1);
-}
+const queryAPI = (endpoint) => {
+  let total = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
+  return total;
+};
 
-export { queryAPI, weakMap };
+export { weakMap, queryAPI };
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,15 @@
 export default function cleanSet(set, startString) {
-  const valuesArray = [...set];
-  const filteredValues = valuesArray.filter((value) => value.startsWith(startString));
-  return filteredValues.join('-');
+  let res = '';
+
+  if (!startString || !startString.length) {
+    return res;
+  }
+
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      res += `${element.slice(startString.length)}-`;
+    }
+  }
+
+  return res.slice(0, -1);
 }
